9afdc2a2efbb413e7428f6575c8ed3519539a002
Vaibhav <vav8085@gmail.com>
2018-01-20 22:05:53 -0600

    hello kotlin

---
+++ Projects/HelloKotlin/app/src/main/java/kot1yn/vav/com/hellokotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package kot1yn.vav.com.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
