7fa5aa6d141bb9902c0dafec3076882a8d30323a
Vaibhav Pathak <vav8085@gmail.com>
2019-12-06 15:24:40 -0500

    types

--- Projects/KotlinApprentice/src/chapter_03/TypesAndOperators.kt
+++ Projects/KotlinApprentice/src/chapter_03/TypesAndOperators.kt
@@ -40,5 +40,42 @@ fun multilineStrings(){
     println(myDetails)
 }
 fun pairsAndTriples(){
-    
+    //ToDo pairs and triples can be used to save 2 or 3 values of data, for more than 3 values use kotlin data classes
+
+    //ToDO Pairs
+    //ToDO pairs and triples once created cannot be changed. THe values in them remain the same. If you want to change them
+    //ToDo then you can extract the value and change it and then save it in a new triple or pair
+    val coordinates: Pair<Int, Int> = Pair(2,3)
+    val coordinates1 = Pair(2,3)
+    val coordinates2 = 2 to 3
+    val coordinates3 = 2 to null
+    //fetching pair values
+    val x = coordinates.first
+    val y = coordinates.second
+
+    //ToDO Triples
+    val coordinates3D = Triple(2,3,4)
+    val x1 = coordinates3D.first
+    val x2 = coordinates3D.second
+    val x3 = coordinates3D.third
+}
+
+fun anyUnitNothing(){
+    //ToDO all the types in kotlin except nullables extend Any, ***notice what Control Shift + P returns for these
+    val a: Any = 20 //its an int
+    val b: Any = 20.23 //its a double here
+    unitFunction()
+}
+
+fun unitFunction(): Unit {
+    println("A function that returns nothing has return type unit just like void in java")
+}
+
+/**
+ * This function never returns
+ */
+fun nothingFunction(): Nothing{
+    while (true){
+
+    }
 }
