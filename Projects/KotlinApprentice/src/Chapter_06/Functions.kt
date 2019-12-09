3661fe5e5dd87374e2b289b40abb4b8c43ff9700
Vaibhav Pathak <vav8085@gmail.com>
2019-12-09 13:52:11 -0500

    functions over

--- Projects/KotlinApprentice/src/Chapter_06/Functions.kt
+++ Projects/KotlinApprentice/src/Chapter_06/Functions.kt
@@ -1,5 +1,7 @@
 package Chapter_06
 
+import java.util.concurrent.atomic.DoubleAdder
+
 fun main(){
     multiplyByFive(input = 6) // named argument
     multiplyByFive()
@@ -9,6 +11,13 @@ fun main(){
     println(number)
     println(square)
     println(multiplyInferred(3,9))
+    println(printFullName("vaibhav","pathak"))
+    val fullName = printFullName("Vaibhav", "Pathak")
+    val (firstName, lastName) = fullName("Vaibhav", "Pathak")
+    println("$firstName $lastName")
+    kotlinAssigningFunctionsToVar()
+    //nonReturningFunctions(5,6)
+    challenges()
 }
 fun multiplyByFive(input: Int = 4){ //Default value
     println("Multiplying $input by 5 returns ${input*5}")
@@ -42,5 +51,59 @@ fun miniExercise(){
 fun printFullName(firstName: String, lastName: String): String{
     //return firstName + " " + lastName
     return "$firstName $lastName"
+}
+fun fullName(firstName: String, lastName: String): Pair<String, String> {
+    return Pair(firstName, lastName)
+}
+
+fun kotlinAssigningFunctionsToVar(){
+    //:: is method reference operator here
+    var function1: (Int, Int) -> Int = ::add
+    function1(2,3) //you can use the function variables just like the function itself
+    println(addNumbers(5,6,::add)) //passing function to another function
+}
+fun add(a: Int, b: Int): Int{
+ return a + b
+}
+
+fun addNumbers(a: Int, b: Int,function: (Int,Int)-> Int): Int{
+    return function.invoke(a,b)
+}
+
+/**
+ * A non returning function is good when we want a function to never return . these kind of functions are good when
+ * we want to keep taking some inputs from user and actually never end the function, the entire thing starts with
+ * some default input and just keep running forever
+ * We do this by making the function return type as Nothing
+ */
+fun nonReturningFunctions(a: Int, b: Int): Nothing{
+    while (true){
+        //run an event loop or some never ending thing here
+    }
+}
+fun challenges(){
+    println(isPrime(8))
+    println(fibonacci(3))
+}
+fun isDivisible(divisor: Int, number: Int): Int{
+    return number % divisor
+}
+fun isPrime(number: Int): Boolean{
+    if(number<=1){
+        return false
+    }
+    for(i in 2..(number-1)){
+        if(isDivisible(i,number)==0){
+            return false
+        }
+    }
+    return true
+}
+
+fun fibonacci(number: Int): Int{
+    if(number <= 2){
+        return 1
+    }
+    return (fibonacci(number-1) + fibonacci(number-2))
 }
 
