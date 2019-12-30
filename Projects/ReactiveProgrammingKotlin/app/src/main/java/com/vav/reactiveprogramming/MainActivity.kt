9134efce3b9618a493e1f98089a9373143bca234
Vaibhav Pathak <vav8085@gmail.com>
2019-12-30 11:22:32 -0500

    observables and subscribing to them

--- Projects/ReactiveProgrammingKotlin/app/src/main/java/com/vav/reactiveprogramming/MainActivity.kt
+++ Projects/ReactiveProgrammingKotlin/app/src/main/java/com/vav/reactiveprogramming/MainActivity.kt
@@ -2,11 +2,33 @@ package com.vav.reactiveprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Observer
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        observables()
+    }
+
+    private fun observables() {
+        //creating an observable using just
+        val intObservable: Observable<Int> = Observable.just(1)
+        //crearting an observable with 10 items max, it will emit 10 items
+        val intsObservable: Observable<Int> = Observable.just(1,2,3,4,5,6,7,8,9,0)
+        //This observable will emit a single item which will be a list
+        val listObservable = Observable.just(listOf(1,2,3,4,5,6,7))
+        //fromIterable creates an observable which emits items in the list one at a time
+        val observableFromIterable = Observable.fromIterable(listOf(1,2,3,4,5))
+
+        //Subscribing to the observables
+        observableFromIterable.subscribe({ println(it)})
+        println("list observable values")
+        listObservable.subscribe({
+            it.forEach { println(it) }
+        })
     }
 }
